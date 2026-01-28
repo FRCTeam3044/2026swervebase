fce0eac52c44b6cf1385e80aff0c32d47a8d6813
manmadeofpolkadots <dredpirateliam@gmail.com>
2026-01-27 20:36:21 -0500

    Set up the turret subsystem

---
+++ src/main/java/frc/robot/subsystems/drive/turret/TurretIO.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems.drive.turret;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface TurretIO {
+  @AutoLog
+  public static class TurretIOInputs {
+    public double angle = 0.0;
+    public double absEncoderOne = 0.0;
+    public double absEncoderTwo = 0.0;
+    public double relEncoder = 0.0;
+  }
+
+  public default void setAngle(double angle) {}
+
+  public default void updateInputs(TurretIOInputs inputs) {}
+}
