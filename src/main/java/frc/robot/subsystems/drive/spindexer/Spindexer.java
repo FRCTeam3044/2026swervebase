98470641f0cafb9df31b79e84123c58d0d15b9fa
manmadeofpolkadots <dredpirateliam@gmail.com>
2026-01-27 20:52:06 -0500

    spindexter outline was finished

---
+++ src/main/java/frc/robot/subsystems/drive/spindexer/Spindexer.java
@@ -0,0 +1,27 @@
+package frc.robot.subsystems.drive.spindexer;
+
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class Spindexer extends SubsystemBase {
+  private final SpindexerIO io;
+  private final SpindexerIOInputsAutoLogged inputs = new SpindexerIOInputsAutoLogged();
+
+  public Spindexer(SpindexerIO io) {
+    this.io = io;
+  }
+
+  @Override
+  public void periodic() {
+    io.updateInputs(inputs);
+    Logger.processInputs("Spindexer", inputs);
+  }
+
+  public Command setSpeed(double speed) {
+    return Commands.run(() -> io.setSpeed(speed), this);
+  }
+}
