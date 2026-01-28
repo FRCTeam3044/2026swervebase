b407680285f1ca15b53b452ab606697963506406
MarcusFlusche <fluschemarcus@gmail.com>
2026-01-27 20:11:36 -0500

    Finished outline

---
+++ src/main/java/frc/robot/statemachine/States/NeutralZone.java
@@ -0,0 +1,10 @@
+package frc.robot.statemachine.States;
+
+import frc.robot.statemachine.StateMachine;
+import me.nabdev.oxidation.State;
+
+public class NeutralZone extends State{
+    public NeutralZone(StateMachine StateMachine) {
+        super(StateMachine);
+    }
+}
