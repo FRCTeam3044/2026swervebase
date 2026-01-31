ad14db4615149df2eb4437f12260313937c7c66b
MarcusFlusche <fluschemarcus@gmail.com>
2026-01-30 19:00:09 -0500

    Fix shooter casing

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSpark.java
@@ -0,0 +1,9 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterIOSpark implements ShooterIO {
+  /*  private final SparkFlex motor = new SparkFlex(0, MotorType.kBrushless);
+
+  private final SparkFlex motor2 = new SparkFlex(1, MotorType.kBrushless);
+
+  private RelativeEncoder encoder = motor.getEncoder();*/
+}
