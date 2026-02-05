4998baa8d8f422155f80934b5b0ef98a245fbe52
manmadeofpolkadots <dredpirateliam@gmail.com>
2026-02-04 20:30:38 -0500

    push shooter with proper casing

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.shooter;
+
+public class ShooterConstants {
+  public static final int kMotorPort = 0;
+  public static final int kMotorPort2 = 1;
+  public static final int currentLimit = 40;
+}
