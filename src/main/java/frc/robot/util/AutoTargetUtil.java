8527591aeb9d65e7ed49aa0f1dfa73a948545dcf
MarcusFlusche <fluschemarcus@gmail.com>
2026-02-06 20:56:09 -0500

    Made auto target utils and state work

---
+++ src/main/java/frc/robot/util/AutoTargetUtil.java
@@ -0,0 +1,36 @@
+package frc.robot.util;
+
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.util.AllianceUtil.AllianceColor;
+
+public class AutoTargetUtil {
+    private Drive drive;
+
+    final double redSideLine = 4.57482575;
+    final double blueSideLine = 11.96622575;
+
+    public boolean inNeutralZone() {
+        if (drive.getPose().getX() > redSideLine && drive.getPose().getX() < blueSideLine) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean inAllianceZone() {
+        AllianceColor allianceColor = AllianceUtil.getAlliance();
+        if ((drive.getPose().getX() < redSideLine && allianceColor == AllianceColor.RED)
+                || (drive.getPose().getX() > blueSideLine && allianceColor == AllianceColor.BLUE)) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean inOpponentZone() {
+        AllianceColor allianceColor = AllianceUtil.getAlliance();
+        if ((drive.getPose().getX() < redSideLine && allianceColor == AllianceColor.BLUE)
+                || (drive.getPose().getX() > blueSideLine && allianceColor == AllianceColor.RED)) {
+            return true;
+        }
+        return false;
+    }
+}
