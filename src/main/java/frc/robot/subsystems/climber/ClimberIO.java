05e84f99634314c02f69a643906d63cda2497ceb
colinnowak6 <colinnowak6@Gmail.com>
2026-02-06 20:58:55 -0500

    CLimber subsystem

--- src/main/java/frc/robot/subsystems/climber/ClimberIO.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIO.java
@@ -2,8 +2,11 @@ package frc.robot.subsystems.climber;
 
 public interface ClimberIO {
     public static class ClimberIOInputs {
+
     }
-    // Movemoter, set speed, position, 
-    public default void setSpeed(double speed) {}
+
+    // Set speed;
+    public default void setSpeed(double speed) {
+    }
 
 }
