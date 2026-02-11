4c8c767fbc1d0b67701e82b25cfb644a9187dc1a
colinnowak6 <colinnowak6@Gmail.com>
2026-02-11 18:58:41 -0500

    Climber

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSpark.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSpark.java
@@ -1,26 +1,28 @@
 package frc.robot.subsystems.climber;
 import static frc.robot.util.SparkUtil.ifOk;
 
-import com.ctre.phoenix6.sim.TalonFXSimState.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkLimitSwitch;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import static frc.robot.subsystems.climber.ClimberConstants.*;
 
 import edu.wpi.first.math.MathUtil;
 import me.nabdev.oxconfig.ConfigurableParameter;
 
 
 public class ClimberIOSpark implements ClimberIO {
-    private final SparkMax motor = new SparkMax(ClimberConstants.canId, MotorType.Brushless); //Fill in later
+    private final SparkMax motor = new SparkMax(canId, MotorType.kBrushless); //Fill in later
 
     private final RelativeEncoder climbEncoder = motor.getEncoder();
     private final SparkLimitSwitch bottomLimit = motor.getReverseLimitSwitch();
     private final ConfigurableParameter<Double> runSpeedUp = new ConfigurableParameter<>(0.0, "Climber Motor Running Speed");
     // No current angle because it will be 0'd
     private final ConfigurableParameter<Double> runSpeedDown = new ConfigurableParameter<>(-0.0, "Climber Motor Running Speed");
+
     public void updateInputs(ClimberIOInputs inputs) {
+        inputs.bottomLimitPressed = bottomLimit.isPressed();
         if (bottomLimit.isPressed()) {
-            inputs.bottomLimitPressed = true;
             climbEncoder.setPosition(0); //Reset encoder to 0 when bottom limit is pressed
         }
         inputs.currentPosition = climbEncoder.getPosition();
