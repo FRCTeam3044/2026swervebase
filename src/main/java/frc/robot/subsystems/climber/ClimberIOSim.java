06283114877569f7f143c9c49bf4f77a93aa2d91
manmadeofpolkadots <dredpirateliam@gmail.com>
2026-02-12 19:36:46 -0500

    finish climber

--- src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOSim.java
@@ -6,13 +6,7 @@ import com.revrobotics.spark.SparkMax;
 // Not finished
 
 public class ClimberIOSim implements ClimberIO {
-<<<<<<< HEAD
   private SparkMax sparkMax = new SparkMax(ClimberConstants.canId, null); // Fill in Later
   // private SparkMaxSim sparkMaxSim = new SparkMaxSim(sparkMax, gearBox);
   private RelativeEncoder encoder = sparkMax.getEncoder();
-=======
-        private SparkMax sparkMax = new SparkMax(ClimberConstants.canId, null); // Fill in Later
-        // private SparkMaxSim sparkMaxSim = new SparkMaxSim(sparkMax, gearBox);
-        private RelativeEncoder encoder = sparkMax.getEncoder();
->>>>>>> origin/main
 }
