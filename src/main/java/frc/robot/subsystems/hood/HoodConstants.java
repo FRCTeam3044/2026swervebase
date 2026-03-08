1e047c0e413db55be0c78b89d6ad53d7f8c41597
nab138 <nab@nabdev.me>
2026-03-08 16:57:22 -0400

    One auto aim

--- src/main/java/frc/robot/subsystems/hood/HoodConstants.java
+++ src/main/java/frc/robot/subsystems/hood/HoodConstants.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems.hood;
 public class HoodConstants {
   public static final int canId = 4;
   public static final int freeCurrentLimit = 30;
-  public static final int stallCurrentLimit = 4;
+  public static final int stallCurrentLimit = 6;
 
   public static final double maxPosition = 41.0;
   public static final double minPosition = 4.6;
